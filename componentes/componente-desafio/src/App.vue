17d892e80142545b190c84ee23923040ad78763d
francois-mesa <francois.michell@mesainc.com.br>
2019-10-01 17:09:00 -0300

    modulo componentes

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <app-navigation />
+
+    <app-toolbar />
+
+    <v-content>
+      <app-content />
+    </v-content>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Drawer from "@/components/template/Drawer";
+import Footer from "@/components/template/Footer.vue";
+import Toolbar from "@/components/template/Toolbar.vue";
+import Content from "@/components/widgets/Content.vue";
+
+export default {
+  components: {
+    "app-navigation": Drawer,
+    "app-footer": Footer,
+    "app-toolbar": Toolbar,
+    "app-content": Content
+  }
+};
+</script>
---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- <app-contador></app-contador> -->
+    <app-contadores/>
+    <button>?</button>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+</style>
